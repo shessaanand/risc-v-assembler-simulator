f530e55c39e43200edaa945321290bf0fefd2371
Shessaanand Siva Sivamurugan <shessaasiva@gmail.com>
2026-03-08 13:45:01 +0530

    update second_pass.py

--- co_2026_evaluation_framework_release/SimpleAssembler/second_pass.py
+++ co_2026_evaluation_framework_release/SimpleAssembler/second_pass.py
@@ -1,25 +1,22 @@
 from errors import AssemblerError
-from tokenizer import splitIntoTokens
-from resolver import resolveOffset
-from validator import checkRegister,checkImmRange
-from opcodes import knownOpcodes,opcodeType,maxInstructions
-from encode_r import packRType
-from encode_i import packIType
-from encode_s import packSType
-from encode_b import packBType
-from encode_u import packUType
-from encode_j import packJType
+from parsingF import tokensplit, checkreg, checkirange
+from symbolsF import resolveOffset
+from opcodes import knownopcodes, opcodeType, maxi
+
+from R_I_encoding import packRType, packIType
+from B_S_encoding import packSType, packBType
+from U_J_encoding import packUType, packJType
 
 
-def generateBinary(instructionList, labelMap):
-    if len(instructionList) > maxInstructions:
-        raise AssemblerError(0, f"Program too large: {len(instructionList)} instructions exceed the maximum of {maxInstructions}")
-    binaryOutput= []
-    for lineNumber,currentAddr,inst,instrText in instructionList:
+def Binary(instructionList, labelMap):
+    if len(instructionList) > maxi:
+        raise AssemblerError(0, f"Program is {len(instructionList)} lines, its exceeding {maxi} lines")
+    binaryop= []
+    for lineno,currentadd,inst,instrtxt in instructionList:
         try:
-            inst, operands = splitIntoTokens(instrText)
-            if inst not in knownOpcodes: 
-              raise AssemblerError(lineNumber, f"Unknown opcode: '{inst}'")
+            inst, operands = tokensplit(instrtxt)
+            if inst not in knownopcodes: 
+              raise AssemblerError(lineno, f"Unknown opcode: '{inst}'")
             if inst == "nop":
                 inst = "addi"
                 operands = ["x0", "x0", "0"]
@@ -28,58 +25,64 @@ def generateBinary(instructionList, labelMap):
 
             if iType == 'R':
                 if len(operands) != 3: 
-                  raise AssemblerError(lineNumber, f"'{inst}' expects 3 operands")
-                for reg in operands: checkRegister(reg, lineNumber)
+                  raise AssemblerError(lineno, f"'{inst}' requires 3 operands")
+                for reg in operands: checkreg(reg, lineno)
                 binary = packRType(inst, operands)
 
             elif iType == 'I':
                 if inst in ("lw", "jalr") and len(operands) == 2:
-                    checkRegister(operands[0], lineNumber)
-                    immStr, rs1 = operands[1].split('(')
-                    checkRegister(rs1.rstrip(')'), lineNumber)
-                    checkImmRange(inst, int(immStr, 0), lineNumber)
+                    checkreg(operands[0], lineno)
+                    immstr, rs1 = operands[1].split('(')
+                    checkreg(rs1.rstrip(')'), lineno)
+                    checkirange(inst, int(immstr, 0), lineno)
                 else:
-                    if len(operands)!= 3: raise AssemblerError(lineNumber, f"'{inst}' expects 3 operands")
-                    checkRegister(operands[0], lineNumber)
-                    checkRegister(operands[1],lineNumber)
-                    checkImmRange(inst, int(operands[2],0),lineNumber)
+                    if len(operands)!= 3:
+                        raise AssemblerError(lineno, f"'{inst}' requires 3 operands")
+                    checkreg(operands[0], lineno)
+                    checkreg(operands[1],lineno)
+                    checkirange(inst, int(operands[2],0),lineno)
                 binary = packIType(inst,operands)
 
             elif iType == 'S':
-                if len(operands)!= 2: raise AssemblerError(lineNumber, f"'{inst}' expects 2 operands")
-                checkRegister(operands[0],lineNumber)
-                immStr,rs1 = operands[1].split('(')
-                checkRegister(rs1.rstrip(')'),lineNumber)
-                checkImmRange(inst,int(immStr, 0), lineNumber)
+                if len(operands)!= 2:
+                    raise AssemblerError(lineno, f"'{inst}' requires 2 operands")
+                checkreg(operands[0],lineno)
+                immstr,rs1 = operands[1].split('(')
+                checkreg(rs1.rstrip(')'),lineno)
+                checkirange(inst,int(immstr, 0), lineno)
                 binary=packSType(inst,operands)
 
             elif iType== 'B':
-                if len(operands) != 3: raise AssemblerError(lineNumber, f"'{inst}' expects 3 operands")
-                checkRegister(operands[0], lineNumber)
-                checkRegister(operands[1], lineNumber)
-                branchOffset = resolveOffset(operands[2], currentAddr, labelMap, lineNumber)
-                checkImmRange(inst, branchOffset, lineNumber)
+                if len(operands) != 3:
+                    raise AssemblerError(lineno, f"'{inst}' requires 3 operands")
+                checkreg(operands[0], lineno)
+                checkreg(operands[1], lineno)
+                branchOffset = resolveOffset(operands[2], currentadd, labelMap, lineno)
+                checkirange(inst, branchOffset, lineno)
                 binary = packBType(inst, operands[:2], branchOffset)
 
             elif iType=='U':
-                if len(operands) != 2: raise AssemblerError(lineNumber, f"'{inst}' expects 2 operands")
-                checkRegister(operands[0], lineNumber)
-                checkImmRange(inst, int(operands[1], 0), lineNumber)
+                if len(operands) != 2:
+                    raise AssemblerError(lineno, f"'{inst}' requires 2 operands")
+                checkreg(operands[0], lineno)
+                checkirange(inst, int(operands[1], 0), lineno)
                 binary = packUType(inst, operands)
 
             elif iType=='J':
-                if len(operands) != 2: raise AssemblerError(lineNumber, f"'{inst}' expects 2 operands")
-                checkRegister(operands[0], lineNumber)
-                jumpOffset = resolveOffset(operands[1], currentAddr, labelMap, lineNumber)
-                checkImmRange(inst, jumpOffset, lineNumber)
+                if len(operands) != 2:
+                    raise AssemblerError(lineno, f"'{inst}' requires 2 operands")
+                checkreg(operands[0], lineno)
+                jumpOffset = resolveOffset(operands[1], currentadd, labelMap, lineno)
+                checkirange(inst, jumpOffset, lineno)
                 binary = packJType(inst, [operands[0]], jumpOffset)
 
-            if len(binary)!= 32: raise AssemblerError(lineNumber, f"Encoder produced {len(binary)} bits for '{inst}'")
-            binaryOutput.append(binary)
+            if len(binary)!= 32:
+                raise AssemblerError(lineno, f"Encoder produced {len(binary)} bits for '{inst}'")
+            binaryop.append(binary)
 
         except AssemblerError:
             raise
         except Exception as e:
-            raise AssemblerError(lineNumber, str(e))
+            raise AssemblerError(lineno, str(e))
 
-    return binaryOutput
+    return binaryop
